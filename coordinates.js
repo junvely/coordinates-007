49fbfcb6b078727907438b021e1a66490cf1c47f
junvely <junvely97@gmail.com>
2022-04-23 20:08:19 +0900

    Update performance
    - Change position to translate in JavaScript to improve performance

--- coordinates.js
+++ coordinates.js
@@ -3,17 +3,20 @@ const target = document.querySelector(".target");
 const coordCon = document.querySelector(".coordinates");
 const coordY = document.querySelector(".coord_Y");
 const coordX = document.querySelector(".coord_X");
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 
 // 마우스 이벤트
 
 window.addEventListener("mousemove", (e) => {
   const x = e.clientX;
   const y = e.clientY;
-  target.style.top = y + "px";
-  target.style.left = x + "px";
-  coordCon.style.top = y + "px";
-  coordCon.style.left = x + "px";
-  coordX.style.top = y + "px";
-  coordY.style.left = x + "px";
+  coordX.style.transform = `translateX(${x}px)`;
+  coordY.style.transform = `translateY(${y}px)`;
+  target.style.transform = `translate(${x - targetHalfWidth}px, ${
+    y - targetHalfWidth
+  }px)`;
+  coordCon.style.transform = `translate(${x}px, ${y}px)`;
   coordCon.innerHTML = `${x}px , ${y}px`;
 });
